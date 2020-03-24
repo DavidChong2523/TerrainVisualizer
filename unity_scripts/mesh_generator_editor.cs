ba48a0c9aac1ce201c2d180336ff8e644fb792c3
David Chong <davidchong2523@gmail.com>
2020-03-23 23:10:23 -0700

    uploaded unity scripts

---
+++ unity_scripts/mesh_generator_editor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(mesh_generator))]
+public class mesh_generator_editor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        mesh_generator meshGen = (mesh_generator)target;
+        DrawDefaultInspector();
+
+        if (DrawDefaultInspector())
+        {
+            if (meshGen.autoUpdate)
+                meshGen.generate_mesh();
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            meshGen.generate_mesh();
+        }
+    }
+}
